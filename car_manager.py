36c3a64501686ac9d4ba27c75dfca4d67ad75ac5
NicoleAMS <nicole.sentis@gmail.com>
2021-06-25 14:37:28 +0200

    creates cars and car behaviour

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(1, 2, 1)
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.goto(300, random.randint(-250, 250))
+
+    def move(self):
+        new_x = self.xcor() - STARTING_MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
--- main.py
+++ main.py
@@ -14,9 +14,19 @@ screen.listen()
 screen.onkey(player.move, "Up")
 
 game_is_on = True
+game_loop = 0
+cars = []
+
 while game_is_on:
+    if game_loop % 10 == 0:
+        new_car = CarManager()
+        cars.append(new_car)
+    for car in cars:
+        car.move()
+        if car.xcor() < -350:
+            cars.remove(car)
     time.sleep(0.1)
     screen.update()
-
+    game_loop += 1
 
 screen.exitonclick()
